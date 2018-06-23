7315cb388e5c48f65dbb54f01c724d3e2220c42b
kalepawan101 <kalepawan101@gmail.com>
2018-06-24 01:02:31 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  Name:string = "Marvellous";
+  tagline:string = "Educating For Better Tommorow";
+  year:number=6;
+}
